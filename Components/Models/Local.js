29263577326cea62dcf29741fa60734f47f27f39
Zulkarnain Shah <brandon.zulu@gmail.com>
2017-02-16 00:40:41 +0530

    UserInfo modelling and Saving
    UserInfo model mapped from server JSON and saved in AsyncStorage

---
+++ Components/Models/Local.js
@@ -0,0 +1,22 @@
+/**
+ * Created by zulkarnainshah on 15/02/17.
+ */
+
+
+export default class Local extends Object{
+    auth0id:string;
+    id:int;
+    username:string;
+    created_at:moment;
+    updated_at:moment;
+    firstname:string;
+    lastname:string;
+    middlename:string;
+    picture:string;
+    provider:string;
+    auth0nickname:string;
+    auth0email:string;
+    auth0email_verified:int;
+    removed:int;
+
+}
--- Components/UserHomeScreen.js
+++ Components/UserHomeScreen.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
 import { AsyncStorage } from 'react-native';
 import { Text } from 'react-native';
-import { USERTOKEN } from './LocalStorage';
-
+import UserInfo from './Models/UserInfo';
 
 class UserHomeScreen extends Component {
-  state = { userID: '' };
+    state = { userID: '' };
+    componentWillMount(){
+        AsyncStorage.getItem('userId').then((value) => {
+            this.setState({ userID: value });
+            const API_ENDPOINT= 'https://server-dev1.mywardrobe.space/api/v1/userinfo';
+            const userID = value;
+            fetch(API_ENDPOINT,{ method: "GET",
+                headers:{
+                    'Authorization': 'Bearer '+ userID
+                }
+            })
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    console.log(responseJson);
+                    userInfo = new UserInfo();
+                    userInfo.updateValues(responseJson);
+                    AsyncStorage.setItem("userInfo",JSON.stringify(userInfo),null);
+                    // console.log("SUCCESSy");
+                    backInfo = AsyncStorage.getItem("userInfo").then(value);
+                })
+
+        });
 
 
 
+    }
 
+    render(){
 
-  componentWillMount(){
-
-      AsyncStorage.getItem('userId').then((value) => {
-             this.setState({ userID: value });
-          const API_ENDPOINT= 'https://server-dev1.mywardrobe.space/api/auth/userinfo';
-           const userinfo = value;
-           console.log(userinfo);
-           fetch(API_ENDPOINT,{ method: "GET",
-             headers:{
-
-               'Authorization': 'Bearer '+ userinfo
-             }
-           })
-           .then((response) => response.json())
-           .then((responseJson) => {
-             console.log(userinfo);
-               console.log(responseJson);
-
-           })
-
-       });
-
-
-
-}
-
-  render(){
-
-    return(
-    <Text>hello { this.state.userID }
-    </Text>
-   )
-  }
+        return(
+            <Text>hello { this.state.userID }
+            </Text>
+        )
+    }
 }
 
 
