ad4b5cc953581ca7d7f1b32726b220fde0236e91
Zulkarnain Shah <brandon.zulu@gmail.com>
2017-02-20 20:42:24 +0530

    Show single piece

--- Actions/ShowSinglePieceAction.js
+++ Actions/ShowSinglePieceAction.js
@@ -1,31 +1,27 @@
- import { SHOW_SINGLE_PIECE } from './Types';
+import { SHOW_SINGLE_PIECE } from './Types';
 
 export const viewSinglePiece = (idToken, UserId, pieceId) => {
-  return (dispatch) => {
- const API_ENDPOINT = 'https://server-dev1.mywardrobe.space/api/v1/users/'+UserId+'/pieces/'+pieceId;
-   fetch(API_ENDPOINT,{
-     method: 'GET',
+    return (dispatch) => {
+        const API_ENDPOINT = 'https://server-dev1.mywardrobe.space/api/v1/users/'+UserId+'/pieces/'+pieceId;
+        fetch(API_ENDPOINT,{
+            method: 'GET',
 
-   headers:{
-     'Authorization': 'Bearer '+idToken
- }
-}).then((response) => response.json())
- .then((responseJson) => {
-   console.log(responseJson);
-      const singlePiece = responseJson;
+            headers:{
+                'Authorization': 'Bearer '+idToken
+            }
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                const singlePiece = responseJson;
+                retrieveSinglePiece(dispatch, singlePiece)
+            });
+    }
+}
 
-
-    retrieveSinglePiece(dispatch, singlePiece)
-
-    });
-  }
-  }
-
-  export const retrieveSinglePiece = (dispatch, pieceData) => {
+export const retrieveSinglePiece = (dispatch, pieceData) => {
     dispatch({
-      type: SHOW_SINGLE_PIECE,
-      payload: pieceData
-
+        type: SHOW_SINGLE_PIECE,
+        payload: pieceData
     });
 
- }
+}
--- Components/ShowSinglePieceScreen.js
+++ Components/ShowSinglePieceScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {  View, Text } from 'react-native';
+import { View, Text,Image } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import { Card, CardSection, Button, NewButton } from './Common';
@@ -8,68 +8,75 @@ import { viewSinglePiece } from '../Actions';
 class ShowSinglePieceScreen extends Component {
 
 
-  static propTypes = {}
+    static propTypes = {}
 
-  static defaultProps = {}
+    static defaultProps = {}
 
-  constructor(props) {
-    super(props);
-    this.state = { pieces: {} };
-  }
-  componentWillMount() {
-  const id = this.props.id;
-
-
-  const userId = this.props.userId;
-
-  const idToken = this.props.idToken;
-
-     this.props.viewSinglePiece(idToken, userId, id);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { pieces: {} };
+    }
+    componentWillMount() {
+        const id = this.props.id;
 
 
- render() {
- console.log(this.props.piece);
-  return (
-    <View>
-<Text>hello world </Text>
-  </View>
-  );
+        const userId = this.props.userId;
 
- }
+        const idToken = this.props.idToken;
+        this.props.viewSinglePiece(idToken, userId, id);
+    }
+
+
+    render() {
+        // console.log(this.props.piece.piece);
+        if (this.props.piece != null){
+            this.props.piece.piece.image
+            return (
+                <Image style={{ flex: 1 }} source={{ uri: this.props.piece.piece.image }} />
+            );
+        }
+        else {
+            return (
+                <View>
+                  <Text>hello world </Text>
+
+                </View>
+            );
+        }
+    }
 }
 
 const styles = {
-  headerContentStyle: {
-    flexDirection: 'column',
-    justifyContent: 'space-around'
-  },
-  headerTextStyle: {
-    fontSize: 18
-  },
-  thumbnailStyle: {
-    height: 50,
-    width: 50
-  },
-  thumbnailContainerStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 10,
-    marginRight: 10
-  },
-  imageStyle: {
-    height: 300,
-    flex: 1,
-    width: null
-  }
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 };
 
 const mapStateToProps = state => {
-  console.log(state.singlePiece.piece);
-  return {
-    piece: state.singlePiece.piece
+    console.log('danger');
+    return {
+        piece: state.singlePiece.piece
 
-  };
+    };
 };
 
 export default connect(mapStateToProps, { viewSinglePiece })(ShowSinglePieceScreen);
